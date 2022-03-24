a332514e9a5fb218c91a79c655bed17c59cacf77
samuele spadaro <spadaro.samuele@gmail.com>
2022-03-24 15:18:52 +0100

    milestone 1 fatta

---
+++ asset/js/main.js
@@ -0,0 +1,51 @@
+/*Definiamo un array di oggetti che rappresentano i membri del team. Ogni membro ha le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+
+Milestone 1: stampare in console l'elenco dei membri del team, scrivendo separatamente i dettagli di ciascun componente.
+
+Milestone 2: stampare i dati all'interno di un contenitore nella pagina html in modo dinamico, creando per ciascun membro del team un elemento che conterrà i suoi dati.
+BONUS: stilizziamo la sezione realizzando le card di ciascun membro del team, come nel mockup allegato.*/
+
+//creazione array con i dati dei dipendenti 
+
+let datiDipendenti = [
+    {
+       "nome" : "Wayne Barnett",
+       "ruolo" : "Founder e CEO",
+       "img" : "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        "nome" : "Angela Caroll",
+        "ruolo" : "Chief Editor",
+        "img" : "angela-caroll-chief-editor.jpg"
+     },
+     {
+        "nome" : "Walter Gordon",
+        "ruolo" : "Office Manager",
+        "img" : "walter-gordon-office-manager.jpg"
+     },
+     {
+        "nome" : "Angela Lopez",
+        "ruolo" : "Social Media Manager",
+        "img" : "angela-lopez-social-media-manager.jpg"
+     },
+     {
+        "nome" : "Scott Estrada",
+        "ruolo" : "Developer",
+        "img" : "scott-estrada-developer.jpg"
+     },
+     {
+        "nome" : "Barbara Ramos",
+        "ruolo" : "Graphic Designer",
+        "img" : "barbara-ramos-graphic-designer.jpg"
+     },
+    
+]
+
+//stampare in console l'elenco dei membri del team, scrivendo separatamente i dettagli di ciascun componente.
+
+for( i = 0; i < datiDipendenti.length; i++ ){
+    for( let key in datiDipendenti[i] ){
+        console.log(key)
+        console.log(datiDipendenti[i][key])
+    }
+}
